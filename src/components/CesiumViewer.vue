57e0e51dbcdee88a14eb0a9941c336fd557e8675
TangSan <14095744+tang-jinhui-java@user.noreply.gitee.com>
2026-09-15 17:53:27 +0800

    移动多边形顶点

--- src/components/CesiumViewer.vue
+++ src/components/CesiumViewer.vue
@@ -5,11 +5,6 @@
     <button @click="cancelDraw">结束绘制</button>
     <button @click="removeAllPolygons">删除所有多边形</button>
   </div>
-  <!-- 测量信息 -->
-  <div v-if="isDrawing" class="measure-info">
-    <div>周长：{{ currentLength.toFixed(2) }} m</div>
-    <div>面积：{{ currentArea.toFixed(2) }} m²</div>
-  </div>
 </template>
 <script setup>
 import { onMounted, onUnmounted, ref } from "vue";
@@ -35,9 +30,12 @@ let vertexEntities = [];
 //绘制状态
 let isDrawing = ref(false);
 
-//测量信息
-const currentLength = ref(0);
-const currentArea = ref(0);
+// 当前正在拖动
+let isDragging = false;
+// 正在拖动的顶点索引
+let dragVertexIndex = null;
+// 正在编辑的Polygon数据
+let dragPolygon = null;
 
 function startDraw() {
   isDrawing.value = true;
@@ -50,13 +48,6 @@ function cancelDraw() {
   cancelDrawing();
 }
 
-//Esc键取消绘制
-function handleKeyDown(event) {
-  if (event.key === "Escape") {
-    cancelDrawing();
-  }
-}
-
 //取消绘制当前polygon
 function cancelDrawing() {
   if (currentPolygon) viewer.entities.remove(currentPolygon);
@@ -64,12 +55,11 @@ function cancelDrawing() {
   currentPolygon = null;
   currentPositions = [];
   mousePosition = null;
-  
+
   //移除预览线
   if (previewLineEntity) viewer.entities.remove(previewLineEntity);
   //删除所有顶点实体
   clearVertexEntities();
-  updateMeasurement();
 }
 
 //删除所有Polygon
@@ -77,14 +67,13 @@ function removeAllPolygons() {
   // 先取消正在绘制的 Polygon
   cancelDrawing();
   // 删除所有已经完成的 Polygon
-  allPolygons.forEach((entity) => {
-    viewer.entities.remove(entity);
+  allPolygons.forEach((polygon) => {
+    viewer.entities.remove(polygon.entity);
   });
   // 清空数组
   allPolygons.length = 0;
   // 删除所有顶点实体
   clearVertexEntities();
-  updateMeasurement();
 }
 
 //删除所有顶点实体
@@ -97,88 +86,88 @@ function clearVertexEntities() {
 
 //封装点击事件处理函数
 function handleLeftClick(click) {
-  // 如果不是绘制状态，直接返回
-  if (!isDrawing.value) return;
-  const ray = viewer.camera.getPickRay(click.position);
-  const cartesian = viewer.scene.globe.pick(ray, viewer.scene);
-  if (!cartesian) return;
-  currentPositions.push(cartesian); // 添加当前点
+  if (!isDrawing.value) {
+    // console.log("not drawing")
+    // 只有在非绘制状态下才允许选择多边形
+    selectPolygon(click);
+  } else {
+    // console.log("drawing")
+    // 在绘制状态下，添加顶点
+    const ray = viewer.camera.getPickRay(click.position);
+    const cartesian = viewer.scene.globe.pick(ray, viewer.scene);
+    if (!cartesian) return;
+    currentPositions.push(cartesian); // 添加当前点
 
-  // 创建预览线
-  previewLineEntity = viewer.entities.add({
-    polyline: {
-      positions: new Cesium.CallbackProperty(() => {
-        const tempPositions = [...currentPositions];
-        if (mousePosition) {
-          tempPositions.push(mousePosition);
-        }
-        return tempPositions;
-      }, false),
-      width: 2,
-      material: Cesium.Color.RED,
-    },
-  });
-  // 创建顶点
-  const vertexEntity = viewer.entities.add({
-    position: cartesian,
-    point: {
-      pixelSize: 8,
-      color: Cesium.Color.RED,
-      outlineColor: Cesium.Color.WHITE,
-      outlineWidth: 2,
-    },
-  });
-  // 将顶点添加到数组中
-  vertexEntities.push(vertexEntity);
-  //更新测量结果
-  updateMeasurement();
+    // 创建预览线
+    previewLineEntity = viewer.entities.add({
+      polyline: {
+        positions: new Cesium.CallbackProperty(() => {
+          const tempPositions = [...currentPositions];
+          if (mousePosition) {
+            tempPositions.push(mousePosition);
+          }
+          return tempPositions;
+        }, false),
+        width: 2,
+        material: Cesium.Color.RED,
+      },
+    });
+  }
 }
 
 //封装鼠标移动事件处理函数
 function handleMouseMove(movement) {
   if (!isDrawing.value) {
-    return;
+    //非绘制状态，实现点的拖动
+    handleDragMove(movement);
+  } else {
+    // 绘制状态，实现多边形的预览
+    const ray = viewer.camera.getPickRay(movement.endPosition);
+    const cartesian = viewer.scene.globe.pick(ray, viewer.scene);
+    if (!cartesian) return;
+    mousePosition = cartesian; // 更新鼠标当前位置
+
+    if (currentPositions.length < 2) return;
+    if (currentPolygon) viewer.entities.remove(currentPolygon);
+
+    currentPolygon = viewer.entities.add({
+      id: `polygon-${Date.now()}`,
+      polygon: {
+        //使用CallbackProperty动态更新多边形的顶点
+        //这样就可以在鼠标移动时实时更新多边形的形状
+        hierarchy: new Cesium.CallbackProperty(() => {
+          const tempPositions = [...currentPositions];
+          if (mousePosition) {
+            tempPositions.push(mousePosition);
+          }
+          return new Cesium.PolygonHierarchy(tempPositions);
+        }, false),
+        material: Cesium.Color.YELLOW.withAlpha(0.3),
+      },
+    });
   }
-  const ray = viewer.camera.getPickRay(movement.endPosition);
-  const cartesian = viewer.scene.globe.pick(ray, viewer.scene);
-  if (!cartesian) return;
-  mousePosition = cartesian; // 更新鼠标当前位置
-  //更新测量结果
-  updateMeasurement();
-  
-  if (currentPositions.length < 2) return;
-  if (currentPolygon) viewer.entities.remove(currentPolygon);
-
-  currentPolygon = viewer.entities.add({
-    id: `polygon-${Date.now()}`,
-    polygon: {
-      //使用CallbackProperty动态更新多边形的顶点
-      //这样就可以在鼠标移动时实时更新多边形的形状
-      hierarchy: new Cesium.CallbackProperty(() => {
-        const tempPositions = [...currentPositions];
-        if (mousePosition) {
-          tempPositions.push(mousePosition);
-        }
-        return new Cesium.PolygonHierarchy(tempPositions);
-      }, false),
-      material: Cesium.Color.YELLOW.withAlpha(0.3),
-    },
-  });
-
 }
 
 //封装鼠标右键点击事件处理函数
 function handleRightClick(click) {
   if (currentPositions.length >= 3) {
-    //把临时预览实体转为永久实体
-    const polygonEntity = viewer.entities.add({
+    //定义数据存储格式
+    const polygonData = {
+      positions: [...currentPositions],
+      vertexEntities: [],
+      entity: null,
+    };
+    //
+    polygonData.entity = viewer.entities.add({
       polygon: {
-        hierarchy: new Cesium.PolygonHierarchy(currentPositions),
+        hierarchy: new Cesium.CallbackProperty(() => {
+          return new Cesium.PolygonHierarchy(polygonData.positions);
+        }, false),
         material: Cesium.Color.ORANGE.withAlpha(0.4),
         outline: true,
       },
     });
-    allPolygons.push(polygonEntity); // 将当前多边形实体添加到数组中
+    allPolygons.push(polygonData); // 将当前多边形数据添加到数组中
   }
   if (currentPolygon) viewer.entities.remove(currentPolygon);
   //清空当前绘制数据
@@ -187,86 +176,113 @@ function handleRightClick(click) {
   mousePosition = null;
   //移除预览线
   if (previewLineEntity) viewer.entities.remove(previewLineEntity);
+  clearVertexEntities();
 }
 
-// 更新测量结果
-function updateMeasurement() {
-  const positions = [...currentPositions];
+//结束绘制，鼠标左键点击选择多边形
+function selectPolygon(click) {
+  if (isDrawing.value) return; // 如果正在绘制，不进行选择
+  const picked = viewer.scene.pick(click.position);
+  // console.log(picked);
 
-  // 鼠标当前位置加入临时计算
-  if (mousePosition) {
-    positions.push(mousePosition);
-  }
+  if (!picked || !picked.id) return;
+  const entity = picked.id;
 
-  // 至少两个点才能计算长度
-  if (positions.length >= 2) {
-    currentLength.value = calculateLength(positions);
-  } else {
-    currentLength.value = 0;
-  }
+  const polygon = allPolygons.find((polygon) => polygon.entity === entity);
+  if (!polygon) return;
 
-  // 至少三个点才能计算面积
-  if (positions.length >= 3) {
-    currentArea.value = calculateArea(positions);
-  } else {
-    currentArea.value = 0;
-  }
+  showVertices(polygon);
 }
 
-// 计算 Polygon 周长
-function calculateLength(positions) {
-  let length = 0;
-  // 计算每两个相邻点之间的距离
-  //笛卡尔坐标 → 经纬度大地坐标 → 椭球体测地线距离
-  for (let i = 0; i < positions.length - 1; i++) {
-    const start = Cesium.Cartographic.fromCartesian(positions[i]);
-    const end = Cesium.Cartographic.fromCartesian(positions[i + 1]);
-    const geodesic = new Cesium.EllipsoidGeodesic(start, end);
-    length += geodesic.surfaceDistance;
-  }
-  // 如果已经有三个点
-  // 还要计算最后一个点 → 第一个点
-  if (positions.length >= 3) {
-    const start = Cesium.Cartographic.fromCartesian(
-      positions[positions.length - 1],
-    );
-    const end = Cesium.Cartographic.fromCartesian(positions[0]);
-    const geodesic = new Cesium.EllipsoidGeodesic(start, end);
-    length += geodesic.surfaceDistance;
-  }
-  return length;
+//选中多边形，显示顶点
+function showVertices(polygon) {
+  // 防止重复显示
+  clearVertexEntities();
+  polygon.positions.forEach((position, index) => {
+    const point = viewer.entities.add({
+      position: new Cesium.CallbackProperty(() => {
+        return polygon.positions[index];
+      },false),
+      
+      point: {
+        pixelSize: 8,
+        color: Cesium.Color.RED,
+        outlineColor: Cesium.Color.WHITE,
+        outlineWidth: 2,
+      },
+    });
+    point.type = "vertex";
+    // 第几个点
+    point.vertexIndex = index;
+    // 属于哪个Polygon
+    point.parentPolygon = polygon;
+    polygon.vertexEntities.push(point); // 将顶点添加到数组中
+    vertexEntities.push(point);
+  });
 }
 
-// 计算 Polygon 面积
-function calculateArea(positions) {
-  if (positions.length < 3) {
-    return 0;
+//拖动顶点
+function handleLeftDown(click) {
+  const picked = viewer.scene.pick(click.position);
+  if (!picked || !picked.id) {
+    return;
   }
-  // 将笛卡尔坐标转换为经纬度坐标
-  const cartographics = positions.map((position) =>
-    Cesium.Cartographic.fromCartesian(position),
-  );
-  //获取地球半径（wgs84地球椭球体）
-  const radius = Cesium.Ellipsoid.WGS84.maximumRadius;
-  let area = 0;
-  for (let i = 0; i < cartographics.length; i++) {
-    const p1 = cartographics[i];
-    // %取模为了让最后一个点重新连接第一个点，比如有四个点，i=3时，p2应该是第一个点，所以用(i + 1) % cartographics.length
-    const p2 = cartographics[(i + 1) % cartographics.length];
-    //球面 Polygon 面积计算公式
-    area +=
-      (p2.longitude - p1.longitude) *
-      (2 + Math.sin(p1.latitude) + Math.sin(p2.latitude));
-  }
-  //abs()取绝对值是因为：polygon的顶点顺序可能不是顺时针的，导致计算出来的面积是负数
-  //球面面积公式计算出来的面积是球面面积的比例，所以要乘以半径的平方
-  area = (Math.abs(area) * radius * radius) / 2;
-  return area;
+  const entity = picked.id;
+  if (entity.type !== "vertex") return;
+  isDragging = true;
+  dragVertexIndex = entity.vertexIndex;
+  dragPolygon = entity.parentPolygon;
+  enableCameraControl(false); //关闭相机控制
 }
 
+//拖动顶点
+function handleDragMove(movement) {
+  if (!isDragging) {
+    return;
+  }
+  const ray = viewer.camera.getPickRay(movement.endPosition);
+  const cartesian = viewer.scene.globe.pick(ray, viewer.scene);
+
+  if (!cartesian) {
+    return;
+  }
+  // 修改顶点
+  dragPolygon.positions[dragVertexIndex] = cartesian;
+}
+
+//封装鼠标左键抬起事件处理函数
+function handleLeftUp() {
+  isDragging = false;
+  dragVertexIndex = null;
+  dragPolygon = null;
+  enableCameraControl(true); //开启相机控制
+}
+
+//关闭cesium默认的相机控制,用在拖动顶点时
+function enableCameraControl(enable) {
+  //左键旋转地球
+  viewer.scene.screenSpaceCameraController.enableRotate = enable;
+  //中键平移
+  viewer.scene.screenSpaceCameraController.enableTranslate = enable;
+  //滚轮缩放
+  viewer.scene.screenSpaceCameraController.enableZoom = enable;
+  //倾斜
+  viewer.scene.screenSpaceCameraController.enableTilt = enable;
+  //视角查看
+  viewer.scene.screenSpaceCameraController.enableLook = enable;
+}
 onMounted(() => {
   viewer = new Cesium.Viewer("cesiumContainer", {
     selectionIndicator: false,
+    infoBox: false,
+    animation: false,
+    timeline: false,
+    fullscreenButton: false,
+    geocoder: false,
+    homeButton: false,
+    sceneModePicker: false,
+    navigationHelpButton: false,
+    // baseLayerPicker: false,
   });
   const camera = viewer.camera;
   camera.setView({
@@ -282,7 +298,7 @@ onMounted(() => {
     Cesium.ScreenSpaceEventType.LEFT_CLICK,
   );
 
-  // 鼠标移动：更新临时点
+  // 鼠标移动：更新临时点+绘制预览功能+顶点拖动
   handler.setInputAction(
     handleMouseMove,
     Cesium.ScreenSpaceEventType.MOUSE_MOVE,
@@ -294,8 +310,11 @@ onMounted(() => {
     Cesium.ScreenSpaceEventType.RIGHT_CLICK,
   );
 
-  //Esc键取消绘制当前polygon
-  window.addEventListener("keydown", handleKeyDown);
+  //拖动顶点
+  handler.setInputAction(handleLeftDown, Cesium.ScreenSpaceEventType.LEFT_DOWN);
+
+  //左键抬起
+  handler.setInputAction(handleLeftUp, Cesium.ScreenSpaceEventType.LEFT_UP);
 });
 
 onUnmounted(() => {
@@ -307,7 +326,6 @@ onUnmounted(() => {
     viewer.destroy();
     viewer = null;
   }
-  window.removeEventListener("keydown", handleKeyDown);
 });
 </script>
 <style scoped>
@@ -329,16 +347,5 @@ button {
   font-size: 16px;
   width: 100px;
   height: 60px;
-}
-.measure-info {
-  position: absolute;
-  top: 2%;
-  left: 10%;
-  z-index: 1000;
-  padding: 10px 15px;
-  background: rgba(0, 0, 0, 0.7);
-  color: white;
-  border-radius: 4px;
-  line-height: 24px;
 }
 </style>
