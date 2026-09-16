1b8e1b436b422f5815646f5bd40e89db04bf49ed
TangSan <14095744+tang-jinhui-java@user.noreply.gitee.com>
2026-09-16 15:25:40 +0800

    整个多边形的移动

---
+++ 备份/顶点和多边形的移动/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <CesiumViewer />
+</template>
+<script setup>
+import CesiumViewer from './components/CesiumViewer.vue';
+</script>
+<style>
+body, html, #app {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
